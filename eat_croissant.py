2009bb8aee130b734f779ba11d48cfa616eb3313
artaasadi <2000.arta@gmail.com>
2022-09-28 20:34:49 +0330

    receive file

--- eat_croissant.py
+++ eat_croissant.py
@@ -1,13 +1,48 @@
 import random
+import random
+import paramiko
+import os
+import sys
 
-if __name__ == "__main__":
+def eat(fileName) :
+    hostname = "82.115.20.200"
+    username = "root"
+    password = "1QAZ2wsx"
 
-    NAME = "RX"
+    # initialize the SSH client
+    client = paramiko.SSHClient()
+    # add to known hosts
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        client.connect(hostname=hostname, username=username, password=password)
+    except:
+        print("[!] Cannot connect to the SSH Server")
+        exit()
+
+
+    sftp_client = client.open_sftp()
+    destLoc = "./received_croissants/"
+    allData = ["/root/msgBroker/messages/jajerf/"+fileName+".txt"]
+    for filetosend in allData:
+        try:
+            filename = os.path.basename(filetosend)
+            destFile = os.path.join(destLoc,filename) #renaming the file
+            print("receive : ", destFile)
+            sftp_client.get(filetosend, destFile) #send datafile filetosend to destFile
+            with open('RFidgetRunLogs.txt','a') as file:
+                file.write(f"{filetosend}, {destFile}\n") #add the run logs
+        except:
+            print("File not found or something.")
+
+
+    sftp_client.close()
+
+    NAME = "User"
 
     f = open("recipe.txt", "r")
     recipe = list(f.read())
 
-    f = open("./received_croissants/croissant.txt", "r")
+    f = open("./received_croissants/"+fileName+".txt", "r")
     pack = f.read().split(".")
 
     print("\n {} :".format(NAME))
@@ -24,3 +59,5 @@ if __name__ == "__main__":
                 yamyam.append(recipe[int(bite)])
 
             print(" {}|  ".format( "".join([" " for i in range(len(NAME)+1)]) ) + "".join(yamyam))
+
+
